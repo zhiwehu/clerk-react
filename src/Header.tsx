df02be609e4219758ed95df2ad1959c10a843b9e
Jeffrey Hu <zhiwehu@gmail.com>
2024-04-17 18:19:48 +0800

    init

---
+++ src/Header.tsx
@@ -0,0 +1,21 @@
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
+
+const Header = () => {
+  return (
+    <header>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
+  );
+};
+
+export default Header;
